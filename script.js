60650ca833f4d27c77919e5fed0d1e5fa434164f
lashaNoz <nozadze016@gmail.com>
2019-04-15 20:50:33 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuIcon = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener("click", () => {
+  navbar.classList.toggle("change");
+});
